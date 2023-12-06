62f12dfc88039d4d5a232b619267965f7044e49f
Conrad Belaire <cbelaire@gmail.com>
2023-12-06 11:00:41 -0500

    Completed day 1 part 1

--- d1p1.py
+++ d1p1.py
@@ -1,1 +1,15 @@
-print("hello world")
+f = open("d1p1_input.txt", "r")
+result = 0
+for line in f:
+    current = 0
+    for c in line:
+        if c.isnumeric():
+            current = 10 * int(c)
+            break
+    for c in reversed(line):
+        if c.isnumeric():
+            current += int(c)
+            break
+    result += current
+
+print(result)
