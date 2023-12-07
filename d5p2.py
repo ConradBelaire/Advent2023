4d7f2fbac36305f04337387e814baa690af97944
Conrad Belaire <cbelaire@gmail.com>
2023-12-07 01:06:12 -0500

    cleaned up d5p2 a bit

--- d5p2.py
+++ d5p2.py
@@ -2,7 +2,6 @@ import re
 
 f = open("d5_input.txt", "r")
 
-# Make sure the list doesn't wrap around and read a symbol from bottom for top, for example
 lines = f.readlines()
 num_pattern = r"(?!=\d)(\d+)(?!\d)"
 
@@ -37,57 +36,28 @@ print(seed_pairs)
 
 def travel(front, back, dict_depth=0):
     if dict_depth == len(conversions):
-        #print("finished:", front)
         return [front]
     conv = conversions[dict_depth]
     for k in conv.keys():
         if k[0] <= front <= back <= k[1]:
-            #print("within bounds:", front, back, k, conv[k]-k[0])
             front = conv[k] + front - k[0]
             back = conv[k] + back - k[0]
             return travel(front, back, dict_depth + 1)
         if k[0] <= front < k[1] < back:
             middle = k[1]
-            # if conv[k] + front - k[0] == 0:
-                # ("Front Zero!", front, back, k, conv[k] + front - k[0], dict_depth)
-            #print("front intersect:", front, back, k, conv[k]-k[0])
             return (travel(conv[k] + front - k[0], conv[k] + middle - k[0], dict_depth + 1)
                     + travel(middle, back, dict_depth))
         if front <= k[0] < back < k[1]:
-            #print("back intersect:", front, back, k, conv[k]-k[0])
             middle = k[0]
-            # if conv[k] + middle - k[0] == 0:
-                # print("Back Zero!", front, back, k, conv[k] + middle - k[0], dict_depth)
             return (travel(front, middle, dict_depth)
                     + travel(conv[k] + middle - k[0], conv[k] + back - k[0], dict_depth + 1))
 
-    #print("no bounds:", front, back, conv.keys())
     return travel(front, back, dict_depth + 1)
 
 
 results = []
 for pair in seed_pairs:
-    #print("new seed")
     results += travel(pair[0], pair[1])
 print(results)
 print(min(results))
 
-
-# HELL ZONE
-# Create list of dictionaries
-reverse_conversions = []
-for chunk in chunks[1:]:
-    d = {}
-    for line in chunk[1:]:
-        nums = list(map(int, re.findall(num_pattern, line)))
-        d[(nums[0], nums[0] + nums[2])] = nums[1]
-    reverse_conversions.insert(0, d)
-
-seed = 3869686154
-for d in reverse_conversions:
-    for k in d.keys():
-        # print(seed, k)
-        if k[0] <= seed < k[1]:
-            # print(seed, k[0], k[1], d[k] + seed - k[0])
-            seed = d[k] + seed - k[0]
-# print(seed)
