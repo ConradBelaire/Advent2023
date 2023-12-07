18ea581226b85f250d9177050b6e0b20e3d452ca
Conrad Belaire <cbelaire@gmail.com>
2023-12-06 19:29:07 -0500

    Day 4 part 2

---
+++ d4p2.py
@@ -0,0 +1,24 @@
+import re
+f = open("d4_input.txt", "r")
+
+# Make sure the list doesn't wrap around and read a symbol from bottom for top, for example
+lines = f.readlines()
+num_pattern = r"(?!=\d)(\d+)(?!\d)"
+result = 0
+copies = [1] * len(lines)
+
+for i in range(len(lines)):
+    score = 0
+    line = lines[i].split(":")[1]
+    winners = list(map(int, re.findall(num_pattern, line.split("|")[0])))
+    nums = list(map(int, re.findall(num_pattern, line.split("|")[1])))
+    for num in nums:
+        if num in winners:
+            score += 1
+    for j in range(score):
+        if i + j + 1 > len(lines) : break
+        copies[i+j+1] += copies[i]
+    result += copies[i]
+    print(winners, nums, score)
+
+print(result)
